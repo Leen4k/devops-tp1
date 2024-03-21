31556739596ecf7d2a2a8f393873157df971e291
wathnak <110277798+Leen4k@users.noreply.github.com>
2024-03-21 09:11:37 +0700

    test4

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Hello roth");
-        hello
+        System.out.println("Hello nakie");
     }
 }
